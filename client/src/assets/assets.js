8aecd28a6f65e22fdeafac246ae3dc1eb4b6a1a7
Mike-Testut <michael.testut@gmail.com>
2026-02-19 17:19:36 -0800

    Update NavBar to adjust to smaller screens

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -2,12 +2,16 @@ import PlaceHolderImage from "./PlaceHolderImage.png"
 import SearchIcon from "./SearchIcon.png"
 import BananaLeafBG from "./BananaLeafBG.png"
 import Logo from "./Logo.png"
+import Logo2 from "./Logo2.png"
 import PageBG from "./PageBG.png"
 import CartIcon from "./CartIcon.png"
 import SDLogo from "./SDLogo.png"
 import InstaLogo from "./InstaLogo.png"
 import PlaceHolderProductImage1 from "./PlaceHolderProductImage1.jpg"
 import PlaceHolderProductImage2 from "./PlaceHolderProductImage2.jpg"
+import ProfileIcon from "./ProfileIcon.png"
+import MenuIcon from "./MenuIcon.png"
+import BackIcon from "./BackIcon.jpg"
 
 
 
@@ -16,11 +20,15 @@ export const assets = {
     SearchIcon,
     BananaLeafBG,
     Logo,
+    Logo2,
     PageBG,
     CartIcon,
     SDLogo,
     InstaLogo,
     PlaceHolderProductImage1,
     PlaceHolderProductImage2,
+    ProfileIcon,
+    MenuIcon,
+    BackIcon,
 
 }
