dcbde36e156c2c331996781efa982ead3d152000
Mike-Testut <michael.testut@gmail.com>
2026-03-05 11:10:04 -0800

    create logic to sort products

--- client/src/pages/Collection.jsx
+++ client/src/pages/Collection.jsx
@@ -1,7 +1,6 @@
 import React, {use, useEffect, useState} from 'react'
 import {useParams} from "react-router-dom";
 import {ShopContext} from "../context/ShopContext.jsx";
-import {assets} from "../assets/assets.js";
 import FilterBox from "../components/FilterBox.jsx";
 import Title from "../components/Title.jsx";
 import ProductPreview from "../components/ProductPreview.jsx";
@@ -10,7 +9,7 @@ const Collection = () => {
     const {category} = useParams();
     const { MockProducts } = use(ShopContext);
     const [filterProducts, setFilterProducts] = useState([]);
-
+    const [sortType, setSortType] = useState("newest");
     const [subcategory, setSubcategory] = useState([]);
 
     const toggleSubcategory = (e) => {
@@ -33,13 +32,28 @@ const Collection = () => {
         setFilterProducts(products);
     }
 
-    useEffect(() => {
-        setFilterProducts(MockProducts);
-    },[])
+    const sortProducts = () => {
+        let sortedProducts = filterProducts.slice()
+        switch (sortType) {
+            case "low-high":
+                setFilterProducts(sortedProducts.sort((a, b) => (a.price - b.price)));
+                break;
+            case "high-low":
+                setFilterProducts(sortedProducts.sort((a, b) => (b.price - a.price)));
+                break;
+            default:
+                applyFilter();
+                break;
+        }
+    }
 
     useEffect(() => {
         applyFilter();
     },[category,subcategory]);
+
+    useEffect(() => {
+        sortProducts();
+    },[sortType])
 
 
     return (
@@ -49,10 +63,11 @@ const Collection = () => {
                 <div className="flex justify-between text-base sm:text-2xl mb-4">
                     <Title text2={`${category.toUpperCase()}`} />
                 {/*    Sort Bar */}
-                    <select className={'border-2 border-gray-300 text-sm px-2'}>
-                        <option value={"featured"}>Sort by: Featured</option>
-                        <option value={"low-high"}>Sort by: Low to High</option>
-                        <option value={"high-low"}>Sort by: High to Low</option>
+                    <select onChange={(e)=>setSortType(e.target.value)} className={'border-2 border-gray-300 text-sm' +
+                        ' px-2'}>
+                        <option value={"newest"}>Sort by: Newest</option>
+                        <option value={"low-high"}>Sort by: Price-Low to High</option>
+                        <option value={"high-low"}>Sort by: Price-High to Low</option>
                     </select>
                 </div>
                 <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-6 py-5">
