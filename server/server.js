9155dcb61d320bf054583ea55684f61def9ff879
Mike-Testut <michael.testut@gmail.com>
2026-03-10 20:53:11 -0700

    start backend/ setup server.js

---
+++ server/server.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+
+//App Config
+
+const app = express()
+const port = process.env.PORT || 3000
+
+//Middleware
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+//API endpoints
+app.get('/', (req, res) => {
+    res.send('API is running')
+})
+
+app.listen(port, () => {console.log(`Server started on port ${port}`)})
