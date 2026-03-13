0068555fd9a81aaf742a281ed350f14360c81add
Mike-Testut <michael.testut@gmail.com>
2026-03-13 09:29:07 -0700

    setup cloudinary config

--- server/server.js
+++ server/server.js
@@ -2,12 +2,13 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from "./config/mongodb.js";
-
+import connectCloudinary from "./config/cloudinary.js";
 //App Config
 
 const app = express()
 const port = process.env.PORT || 3000
 connectDB();
+connectCloudinary();
 
 //Middleware
 app.use(cors())
