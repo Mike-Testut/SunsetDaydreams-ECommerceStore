3c2eb951da74c4639871efad2c3cf18d52e1c0d0
Mike-Testut <michael.testut@gmail.com>
2026-03-15 14:00:19 -0700

    setup login user api request

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -4,7 +4,7 @@ import {loginUser, registerUser, adminLogin} from "../controllers/userController
 const userRouter = express.Router();
 
 userRouter.post("/register", registerUser);
-userRouter.post("/userLogin", loginUser);
+userRouter.post("/login", loginUser);
 userRouter.post("/adminLogin", adminLogin);
 
 export default userRouter;
