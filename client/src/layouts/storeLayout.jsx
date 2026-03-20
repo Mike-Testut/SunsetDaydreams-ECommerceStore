c1f97d7f9a81f2affc7bc5fb4a8e5c1e9304c57d
Mike-Testut <michael.testut@gmail.com>
2026-03-20 11:52:32 -0700

    bug fix to scroll to top of page on page change

--- client/src/layouts/storeLayout.jsx
+++ client/src/layouts/storeLayout.jsx
@@ -2,10 +2,12 @@ import { Outlet } from "react-router-dom";
 import NavBar from "../components/NavBar";
 import SearchBar from "../components/SearchBar";
 import Footer from "../components/Footer";
+import ScrollToTop from "../utils/ScrollToTop.js";
 
 const StoreLayout = () => {
     return (
         <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+            <ScrollToTop />
             <NavBar />
             <SearchBar />
             <Outlet />
