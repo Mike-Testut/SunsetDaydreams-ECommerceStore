df6b318aea8bf75112d4638821296d64003d36c5
Mike-Testut <michael.testut@gmail.com>
2026-05-23 11:24:20 -0700

    comment out rate limiter

--- server/server.js
+++ server/server.js
@@ -39,7 +39,7 @@ app.use(cors({
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use(rateLimiter)
+// app.use(rateLimiter)
 
 //API endpoints
 app.use('/api/user/', userRouter);
